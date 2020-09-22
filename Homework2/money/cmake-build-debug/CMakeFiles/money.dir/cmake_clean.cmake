d6e4c643d70eff9463ed77ad80523579e0df2e33
cnarndt <70283233+cnarndt@users.noreply.github.com>
2020-09-21 22:09:09 -0800

    Add files via upload

---
+++ Homework2/money/cmake-build-debug/CMakeFiles/money.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/money.dir/main.cpp.obj"
+  "money.exe"
+  "money.exe.manifest"
+  "money.lib"
+  "money.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/money.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
