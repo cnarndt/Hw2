f4c3ec7052dd4251bec5d328777fea731d937b4e
cnarndt <70283233+cnarndt@users.noreply.github.com>
2020-09-21 22:10:47 -0800

    Add files via upload

---
+++ rice/cmake-build-debug/CMakeFiles/rice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rice.dir/main.cpp.obj"
+  "rice.exe"
+  "rice.exe.manifest"
+  "rice.lib"
+  "rice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rice/main.cpp
@@ -0,0 +1,29 @@
+//Clayton Arndt
+//Homework 2
+//9/20/20
+//CS 201
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+int square[1000] = {1};
+int count = 1;
+
+    for(count; square[count] <= 1000; count++)
+    square[count] = square[count-1]*2;
+    cout<< "It would take " <<count <<" squares to reach 1000 grains\n";
+
+    for(count=1; square[count] <= 1000000; count++)
+        square[count] = square[count-1]*2;
+    cout<< "It would take " <<count <<" squares to reach 1,000,000 grains\n";
+
+    for(count=1; square[count] <= 1000000000; count++)
+        square[count] = square[count-1]*2;
+    cout<< "It would take " <<count <<" squares to reach 1,000,000,000 grains\n";
+
+    return 0;
+
+}
